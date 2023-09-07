1d5469115ffba03c6f4727b41bec33bd91fa67de
DeanElizardo <del@delizardo.com>
2023-09-07 11:00:57 -0400

    basic user input

--- 03_guessing_game/guessing_game/src/main.rs
+++ 03_guessing_game/guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's play a guessing game!");
+    println!("Guess a number...");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Unable to read line.");
+
+    println!("You guessed {guess}");
 }
