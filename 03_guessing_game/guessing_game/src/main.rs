54dd17f8e42cbd46735530166c2807576684e4d5
DeanElizardo <del@delizardo.com>
2023-09-08 10:23:05 -0400

    improved exit logic for guessing game

--- 03_guessing_game/guessing_game/src/main.rs
+++ 03_guessing_game/guessing_game/src/main.rs
@@ -5,31 +5,58 @@ use std::io;
 fn main() {
     println!("Let's play a guessing game!");
 
+    let exit_keyword: String = String::from("quit");
+    let mut exit_semaphore: bool = false;
+    let mut number_of_guesses: u32 = 0;
+    let mut guess: String = String::new();
     loop {
-        let secret_number = rand::thread_rng().gen_range(1..=100);
+        if exit_semaphore {
+            break;
+        }
+        let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-        println!("Guess a number... (Hint: {secret_number})");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Unable to read line.");
-
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please, type a number next time.");
-
-        println!("You guessed {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too low..."),
-            Ordering::Greater => println!("Too high!"),
-            Ordering::Equal => {
-                println!("Just right; you win!");
+        loop {
+            if exit_semaphore {
                 break;
             }
+
+
+            println!("Guess a number... ");
+
+            guess = String::new();
+
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Unable to read line.");
+
+            guess = guess.chars().filter(|&c| !c.is_whitespace()).collect();
+
+            if guess == exit_keyword {
+                exit_semaphore = true;
+                continue;
+            }
+
+            let guess: u32 = match guess.parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+            println!("You guessed {guess}");
+
+            number_of_guesses += 1;
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too low..."),
+                Ordering::Greater => println!("Too high!"),
+                Ordering::Equal => {
+                    println!("Just right; you win!");
+                    exit_semaphore = true;
+                }
+            }
         }
+    }
+
+    if guess != exit_keyword {
+        println!("You made {number_of_guesses} guesses.");
     }
 }
