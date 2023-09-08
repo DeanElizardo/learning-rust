0d24b0a530ac5248a3cbfffa27cfab1527a028d7
DeanElizardo <del@delizardo.com>
2023-09-08 08:05:56 -0400

    Using some pattern matching features and looping in game

--- 03_guessing_game/guessing_game/src/main.rs
+++ 03_guessing_game/guessing_game/src/main.rs
@@ -1,14 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Let's play a guessing game!");
-    println!("Guess a number...");
 
-    let mut guess = String::new();
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Unable to read line.");
+        println!("Guess a number... (Hint: {secret_number})");
 
-    println!("You guessed {guess}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Unable to read line.");
+
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please, type a number next time.");
+
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low..."),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("Just right; you win!");
+                break;
+            }
+        }
+    }
 }
