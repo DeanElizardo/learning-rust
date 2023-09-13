91bc55b3ca85f03a021d14aa9f79d7e43bf5c338
DeanElizardo <del@delizardo.com>
2023-09-13 13:05:13 -0400

    worked up to range syntax

--- 09_the_slice_type/the_slice_type/src/main.rs
+++ 09_the_slice_type/the_slice_type/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s: String = String::from("Hello, world!");
+
+    // Taking a slice causes an immutable borrow
+    let first_word: &str = &s[0..5];
+    let secnd_word: &str = &s[7..12];
+
+    // Implicit mutable borrow is not allowed here!
+    // s.push_str(" howdy.");
+
+    println!("{} {}", first_word, secnd_word);
+
+    s.push_str(" Now that the slices are no longer alive, this push is legal!");
+
+    println!("{}", s);
+
+    /*
+    ***************************************************************************
+    *                             RANGE SYNTAX                                *
+    ***************************************************************************
+    */
+
+    // get the first 3 characters of s
+    let slice_1: &str = &s[..3];
+
+    // get the the characters after the 8th character
+    let slice_2: &str = &s[9..];
+
+    // get a slice of a slice
+    let slice_3: &str = &s[0..15][3..9];
+
+    println!("{}", slice_1);
+    println!("{}", slice_2);
+    println!("{}", slice_3);
 }
